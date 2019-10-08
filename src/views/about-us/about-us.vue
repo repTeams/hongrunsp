5a7dd6c49a3bf3e7fa89d2ca20cbf2f191420ed5
ganke <377264927@qq.com>
2019-10-08 23:57:03 +0800

    提交一下路由的页面

---
+++ src/views/about-us/about-us.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>我这里关于我们</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'aboutUs',
+    data () {
+        return {};
+    },
+    methods: {
+        changeLand (tex) {
+            console.log(tex);
+            this.$i18n.locale = tex;
+        }
+    }
+};
+</script>
+<style land="scss">
+  .active-block{
+    padding:10px;
+  }
+</style>
---
+++ src/views/presentation/presentation.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hello">
+    <h1>我这里是我的提现的啦</h1>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'presentation',
+    data () {
+        return {};
+    },
+    methods: {
+        changeLand (tex) {
+            console.log(tex);
+            this.$i18n.locale = tex;
+        }
+    }
+};
+</script>
+<style land="scss">
+  .active-block{
+    padding:10px;
+  }
+</style>
